a78020445152f9428fc3b75026244afd5218d08f
nekopeko <87629374+nek0peko@users.noreply.github.com>
2021-07-22 11:51:44 +0800

    Update rmv.java

--- rmv.java
+++ rmv.java
@@ -9,14 +9,21 @@ public class rmv {
     
     public static void check(String[] args) {
         if(args.length != 3) {
-            System.out.println("参数输入个数错误！\n");
+            System.out.println("参数输入个数错误！");
+            System.out.println("请使用 'rmv 出库数量 配件ID 仓库ID' 的格式。");
             System.exit(0);
         }
 
         try {
             num = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("出库数量类型错误！\n");
+            System.out.println("出库数量类型错误！");
+            System.exit(0);
+        }
+
+        if(num <= 0) {
+            System.out.println("出库数量应大于0！");
+            System.exit(0);
         }
     }
 
@@ -45,7 +52,7 @@ public class rmv {
             }
             rs.close();
             if(!(pid_exist && wid_exist)) {
-                System.out.println("配件号或仓库号不存在！\n");
+                System.out.println("配件号或仓库号不存在！");
             } else {
                 String sql2 = "update partsrepertory set stock = stock - ? where pid = ? and wid = ? and (stock - ?) >= 0";
                 pstmt = conn.prepareStatement(sql2);
@@ -55,10 +62,10 @@ public class rmv {
                 pstmt.setInt(4, num);
                 int n = pstmt.executeUpdate();
                 if(n == 1) {
-                    System.out.println("出库成功！\n");
+                    System.out.println("出库成功！");
                 }
                 else {
-                    System.out.println("库存不足！\n");
+                    System.out.println("库存不足！");
                 }
                 pstmt.close();
             }
