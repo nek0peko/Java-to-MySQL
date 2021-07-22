2fb32daff6354abaa278a7ba9a4571dd894aa287
nekopeko <87629374+nek0peko@users.noreply.github.com>
2021-07-22 10:24:50 +0800

    Create rmv.java
    example: java -cp .:mysql-connector-java-5.1.46.jar rmv 1 001 A001

---
+++ rmv.java
@@ -0,0 +1,98 @@
+import java.sql.*;
+
+public class rmv {
+    static int num;
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String URL = "jdbc:mysql://192.168.31.250:3306/DB1?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8&useSSL=false";
+    static final String userName = "alice";
+    static final String passWord = "123456";
+    
+    public static void check(String[] args) {
+        if(args.length != 3) {
+            System.out.println("参数输入个数错误！\n");
+            System.exit(0);
+        }
+
+        try {
+            num = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("出库数量类型错误！\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        rmv.check(args);
+        boolean pid_exist = false;
+        boolean wid_exist = false;
+        String PID = args[1];
+        String WID = args[2];
+        Connection conn = null;
+        Statement stmt = null;
+        PreparedStatement pstmt = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(URL, userName, passWord);
+            stmt = conn.createStatement();
+            String sql1 = "select pid, wid from partsrepertory";
+            ResultSet rs = stmt.executeQuery(sql1);
+            while(rs.next()) {
+                if(PID.equals(rs.getString("pid"))) {
+                    pid_exist = true;
+                }
+                if(WID.equals(rs.getString("wid"))) {
+                    wid_exist = true;
+                }
+            }
+            rs.close();
+            if(!(pid_exist && wid_exist)) {
+                System.out.println("配件号或仓库号不存在！\n");
+            } else {
+                String sql2 = "update partsrepertory set stock = stock - ? where pid = ? and wid = ? and (stock - ?) >= 0";
+                pstmt = conn.prepareStatement(sql2);
+                pstmt.setInt(1, num);
+                pstmt.setString(2, PID);
+                pstmt.setString(3, WID);
+                pstmt.setInt(4, num);
+                int n = pstmt.executeUpdate();
+                if(n == 1) {
+                    System.out.println("出库成功！\n");
+                }
+                else {
+                    System.out.println("库存不足！\n");
+                }
+                pstmt.close();
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if(pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException se2) {
+                //Do nothing
+            }
+            try {
+                if(stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                //Do nothing
+            }
+            try {
+                if(conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
